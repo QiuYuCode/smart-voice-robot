5c20e588b55b419577bca73487ab688270d7652f
qiuyu <qiuyucode@163.com>
2026-04-10 17:27:42 +0800

    test: 添加 pyagxarm 依赖和配置，新增左右机械臂测试脚本

---
+++ test/test_left_arm.py
@@ -0,0 +1,74 @@
+import time
+from pprint import pprint
+from pyAgxArm import AgxArmFactory, create_agx_arm_config
+
+
+def main():
+    left_arm_cfg = create_agx_arm_config(robot="nero", comm="can", channel="can_left")
+    left_robot = AgxArmFactory.create_arm(left_arm_cfg)
+
+    # 连接机械臂
+    left_robot.connect()
+    
+    # 重置机械臂
+    # left_robot.reset()
+    # left_robot.enable()
+    
+    # 设置机械臂为正常模式
+    # left_robot.set_normal_mode()
+    
+    # 设置机械臂为主臂模式
+    # left_robot.set_leader_mode()
+    
+    # 设置机械臂为从臂模式
+    # left_robot.set_follower_mode()
+    
+    # 紧急停止机械臂
+    # left_robot.electronic_emergency_stop()
+
+    # 获取从臂/普通模式下的关节角度
+    # while True:
+    #     joint_angles = left_robot.get_joint_angles()
+    #     if joint_angles is not None:
+    #         print("left_joint_angles_msg:", joint_angles.msg)
+    #         print(
+    #             "left_joint_angles_hz:", joint_angles.hz,
+    #             "left_joint_angles_timestamp:", joint_angles.timestamp,
+    #         )
+    #     time.sleep(0.005)
+    
+    # 获取主臂模式下的关节角度
+    # while True:
+    #     joint_angles = left_robot.get_leader_joint_angles()
+    #     if joint_angles is not None:
+    #         print("left_leader_joint_angles_msg:", joint_angles.msg)
+    #         print(
+    #             "left_leader_joint_angles_hz:", joint_angles.hz,
+    #             "left_leader_joint_angles_timestamp:", joint_angles.timestamp,
+    #         )
+    #     time.sleep(0.005)
+
+    # 控制机械臂运动
+    # left_robot.set_speed_percent(20)
+    # 以左臂为参照，右臂的 1 ， 3 ， 5, 6 关节角度与左臂相反
+    # left_robot.move_j([0.1, 0.5, 0.2, 0.2, 0.5, 0.5, 0.5])
+    # left_robot.move_j([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    
+    # time.sleep(0.5)
+    # start_t = time.monotonic()
+    # while True:
+    #     status = left_robot.get_arm_status()
+    #     if status is not None and status.msg.motion_status == 0:
+    #         print("已到达目标位置")
+    #         break
+    #     if time.monotonic() - start_t > 5.0:
+    #         print("等待运动结束超时（5s）")
+    #         break
+    #     time.sleep(0.1)
+    
+    # 读取机械臂状态
+    arm_status = left_robot.get_arm_status()
+    pprint(arm_status)
+    
+if __name__ == "__main__":
+    main()
---
+++ test/test_right_arm.py
@@ -0,0 +1,71 @@
+import time
+
+from pyAgxArm import AgxArmFactory, create_agx_arm_config
+
+
+def main():
+    right_arm_cfg = create_agx_arm_config(robot="nero", comm="can", channel="can_right")
+    right_robot = AgxArmFactory.create_arm(right_arm_cfg)
+
+    # 连接机械臂
+    right_robot.connect()
+    
+    # 重置机械臂
+    # right_robot.reset()
+    # right_robot.enable()
+    
+    # 设置机械臂为正常模式
+    # right_robot.set_normal_mode()
+    
+    # 设置机械臂为主模式
+    # right_robot.set_leader_mode()
+    
+    # 设置机械臂为从臂模式
+    # right_robot.set_follower_mode()
+    
+    # 紧急停止机械臂
+    # right_robot.electronic_emergency_stop()
+    
+
+    # 获取从臂/普通模式下的关节角度
+    # while True:
+    #     joint_angles = right_robot.get_joint_angles()
+    #     if joint_angles is not None:
+    #         print("right_joint_angles_msg:", joint_angles.msg)
+    #         print(
+    #             "right_joint_angles_hz:", joint_angles.hz,
+    #             "right_joint_angles_timestamp:", joint_angles.timestamp,
+    #         )
+    #     time.sleep(0.005)
+        
+    # 获取主臂模式下的关节角度
+    # while True:
+    #     joint_angles = right_robot.get_leader_joint_angles()
+    #     if joint_angles is not None:
+    #         print("right_leader_joint_angles_msg:", joint_angles.msg)
+    #         print(
+    #             "right_leader_joint_angles_hz:", joint_angles.hz,
+    #             "right_leader_joint_angles_timestamp:", joint_angles.timestamp,
+    #         )
+    #     time.sleep(0.005)
+
+    # 控制机械臂运动
+    right_robot.set_speed_percent(20)
+    # 以左臂为参照，右臂的 1 ， 3 ， 5, 6 关节角度与左臂相反
+    right_robot.move_j([-0.1, 0.5, -0.2, 0.2, -0.5, -0.5, 0.5])
+    # right_robot.move_j([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    
+    time.sleep(0.5)
+    start_t = time.monotonic()
+    while True:
+        status = right_robot.get_arm_status()
+        if status is not None and status.msg.motion_status == 0:
+            print("已到达目标位置")
+            break
+        if time.monotonic() - start_t > 5.0:
+            print("等待运动结束超时（5s）")
+            break
+        time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
